c70bd2b0b08a14ca3c3454a256308654575e4bd5
Andy Gott <andy@madebyfieldwork.com>
2018-09-07 18:12:50 +0100

    Fix shell syntax.

--- getcert.sh
+++ getcert.sh
@@ -2,18 +2,18 @@
 
 cd $LE_CERTS_DIR
 
-COMMAND = 'simp_le'
-COMMAND = "$COMMAND --email $LE_EMAIL"
-COMMAND = "$COMMAND -f account_key.json"
-COMMAND = "$COMMAND -f fullchain.pem"
-COMMAND = "$COMMAND -f key.pem"
+COMMAND='simp_le'
+COMMAND="$COMMAND --email $LE_EMAIL"
+COMMAND="$COMMAND -f account_key.json"
+COMMAND="$COMMAND -f fullchain.pem"
+COMMAND="$COMMAND -f key.pem"
 
 for DOMAIN in $LE_DOMAINS
 do
-  COMMAND = "$COMMAND -d $DOMAIN"
+  COMMAND="$COMMAND -d $DOMAIN"
 done
 
-COMMAND = "$COMMAND --default_root $LE_DEFAULT_ROOT"
+COMMAND="$COMMAND --default_root $LE_DEFAULT_ROOT"
 
 echo
 echo "Getting certificate with command:"
